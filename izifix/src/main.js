991f7b4148e1994992d71a0492f433ab118ef142
FranchescoSoto <85808628+FranchescoSoto@users.noreply.github.com>
2022-09-30 04:18:14 -0500

    add: initial imports
    adding initial imports

--- izifix/src/main.js
+++ izifix/src/main.js
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import PrimeVue from "primevue/config";
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
-
+import 'primevue/resources/themes/md-light-deeppurple/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 const app = createApp(App)
-
+app.use(Primevue)
 app.use(createPinia())
 app.use(router)
 
