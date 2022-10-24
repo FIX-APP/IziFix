7eaf64dad70c0b6daf28fdf531ace2d292908859
Cristian Aldair Bonifacio Quispe <89119241+CristianBonifacio@users.noreply.github.com>
2022-10-24 15:06:23 -0500

    add router

--- izifix/src/router/index.js
+++ izifix/src/router/index.js
@@ -1,31 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Login from '../views/shared/general-login.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
+    { 
       path: '/',
-      name: '',
-      component: () => import("../../fixing/pages/register.component.vue"),
+      component: Login
+   },
+   {
+    path: '/login',
+    name: 'login',
+    component: Login
     },
     {
-      path: "/solicitud",
-      name: "solicitud",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../../fixing/pages/solicitud-servicio.component.vue"),
-     },
-     {
-      path: '/register',
-      name: 'register',
-      component: () => import("../../fixing/pages/register.component.vue"),
-    },
-    {
-      path: '/booking',
-      name: 'booking',
-      component: () => import("../../fixing/pages/booking.vue"),
-    },
+      path: '/client',
+      name:'client',
+      component: ()=>import('../components/client/Layout/ClientLayout.vue'),
+      children:[
+        {
+        path:'',
+        redirect:{name:'myappliance'}
+        },
+       {
+          path:'myappliance',
+          name:'myappliance',
+          component: ()=>import(/webpackChunkName:"MyAppliance"/'../views/Client/client-my-appliance.vue'),
+       },
+
+       {
+          path:'appointment',
+          name:'client-appointment',
+          component: ()=>import('../views/Client/client-appointments.vue')
+       },
+       {
+          path:'profile',
+          name:'client-profile',
+          component: ()=>import(/webpackChunkName:"Profile"/'../views/Client/client-profile.vue'),
+          props:(route)=>{
+             const id=Number(route.params.id)
+             return isNaN(id) ? {id:1}:{id:id}
+          }
+       },
+    ]
+   },
   ]
 })
 
