a8b5ed18f77a03ba7f3b1717bf3c16e35980c35d
FranchescoSoto <u201821078@upc.edu.pe>
2022-10-24 09:26:35 -0500

    add: imagez
    adding images for the website

--- izifix/src/App.vue
+++ izifix/src/App.vue
@@ -1,25 +1,9 @@
+
 <template>
-  <div><pv-tabmenu :model="items" class="nav-bar" v-model:visible="drawer"/>  </div>
-
-  <RouterView />
+  <router-view></router-view>
 </template>
-<script>
 
-  export default {
-    data() {
-        return {
-            items: [
-                {label: 'Servicio', icon: 'pi pi-fw pi-wrench', to: '/solicitud'},
-                {label: 'Registrarse', icon: 'pi pi-fw pi-circle-fill', to: '/register'}
-            ]
-        }
-    }
-}
-</script>
-<style>
 
-  .nav-bar{
-    margin-top: 0px;
-    margin-left: 0px;
-  }
+
+<style scoped>
 </style>
