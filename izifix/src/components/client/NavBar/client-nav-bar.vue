ce1de97decd17cbfdf6d09a340cff30add4c651d
jarlh19 <u201616054@upc.edu.pe>
2022-10-24 14:53:47 -0500

    App/components

---
+++ izifix/src/components/client/NavBar/client-nav-bar.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<pv-menu-bar :model="items">
+			<template #start>
+				<img alt="" src="@/assets/img/text-logos/logo_repair.png" height="20" class="mr-2">
+			</template>
+			<template #end>
+                
+			</template>
+		</pv-menu-bar>
+	</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            items: [
+                {
+					label:'My artifacts',
+					icon:'pi pi-home',
+                    to: {name:'myappliance'}
+                },
+                {
+					label:'Appointment',
+					icon:'pi pi-book',
+                    to: {name:'client-appointment'}
+                },
+                {
+					label:'Profile',
+					icon:'pi pi-calendar-times',
+                    to: {name:'client-profile'}
+                },
+              {
+                label:'LogOut',
+                icon:'pi pi-sign-out',
+                to: '/'
+              }
+            ]
+        }
+    }
+}
+
+</script>
+
+<style>
+   
+</style>
