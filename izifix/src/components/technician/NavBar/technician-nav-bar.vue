d7475d4801c84b963dae2a849f36d8a61139f2c2
FranchescoSoto <u201821078@upc.edu.pe>
2022-11-11 18:06:11 -0500

    add: technician components

---
+++ izifix/src/components/technician/NavBar/technician-nav-bar.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<pv-menu-bar :model="items">
+			<template #start>
+				<img alt="" src="@/assets/img/text-logos/logo_repair.png" height="20" class="mr-2">
+			</template>
+			<template #end>
+                
+			</template>
+		</pv-menu-bar>
+	</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            items: [
+                {
+					label:'Appointment',
+					icon:'pi pi-book',
+                    to: {name:'technician-appointment'}
+                },
+                {
+					label:'Profile',
+					icon:'pi pi-calendar-times',
+                    to: {name:'technician-profile'}
+                },
+              {
+                label:'LogOut',
+                icon:'pi pi-sign-out',
+                to: '/'
+              }
+            ]
+        }
+    }
+}
+
+</script>
+
+<style>
+   
+</style>
