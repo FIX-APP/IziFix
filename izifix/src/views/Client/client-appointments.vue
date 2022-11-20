1de858c722dc89834b66bc8d19fb961d58faa147
jarlh19 <u201616054@upc.edu.pe>
2022-11-20 18:34:13 -0500

    Add: new views for appointment

--- izifix/src/views/Client/client-appointments.vue
+++ izifix/src/views/Client/client-appointments.vue
@@ -6,38 +6,69 @@
             <template #header>
                 <div class="table-header">
                     Appointments
-                    <Button icon="pi pi-refresh" ></Button>
+                    <pv-button icon="pi pi-plus" @click="display=true"></pv-button>
+                    <pv-dialog header="Header" v-model:visible="display">
+                        <template #header>
+		                    <h3>Add an appointment</h3>
+	                    </template>
+                        <pv-dropdown v-model="selectedTech" :options="technicians" optionLabel="name"></pv-dropdown>
+                        <pv-input-text class="input-artURL flex justify-content-center" type="text" v-model="desc" placeholder="Add description of your problem" toggleMask  />
+                        <template #footer>
+		                    <pv-button label="No" icon="pi pi-times" class="p-button-text" @click="display=false"/>
+                            <pv-button label="Add artifact" icon="pi pi-check" autofocus @click="addAppointment()"/>
+	                    </template>
+                    </pv-dialog>
                 </div>
             </template>
-            <pv-column field="userId" header="userId"></pv-column>
-            <pv-column field="technicianId" header="Technician ID"></pv-column>
-            <pv-column field="description" header="Description"></pv-column>
+            <pv-column field="user.name" header="User"></pv-column>
+            <pv-column field="technician.name" header="Technician"></pv-column>
         </pv-datatable>
 	</div>
   </div>
+
 </template>
 
 <script>
 import { appointmentService } from '../../core/services/appointmentService';
+import { userService } from '../../core/services/userService';
 export default{
     components:{
     },
     data(){
         return{
-            appointments:[]
+            appointments:[],
+            display:false,
+            technicians:[],
+            selectedTech:null,
+            desc:""
         }
     },
     services:null,
+    techService:null,
     created(){
         this.services=new appointmentService();
+        this.techService=new userService();
     },
     mounted(){
-        this.services.getByUserId(this.$route.params.id).then(value=>{
-            this.appointments=value;
+        this.techService.getAllUsers().then(value=>{
+            this.technicians=value.data;
         });
+        this.services.getAllAppointments().then(value=>{
+            value.data.forEach(element => {
+                if(element.user.id==this.$route.params.id){
+                    this.appointments.push(element);
+                }
+            });
+        })
+    },
+    methods:{
+        addAppointment(){
+            this.services.registerAppointment(parseInt(this.$route.params.id),this.selectedTech.id,this.desc);
+            this.display=false;
+        }
 
-        
     }
+
 }
 </script>
 
